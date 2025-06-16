f46ac7924bf408883e6804b89c4cc3d5c4a2cf34
Lqdat28072005 <23010201@st.phenikaa-uni.edu.vn>
2025-06-16 21:33:02 +0700

    2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   //string
-  const[name,setHoidanit] = useState<string>("hoidanit");
+  const[name,setName] = useState<string>("awqw");
   //number
   const[age,setAge] = useState<number>(20)
   //null
@@ -15,15 +15,43 @@ export default function App() {
     name:"hoidanit",
     age:25
   }]);
-
+  const[todoList,setTodoList] = useState([
+    {id:1,title:"Learn React Native"},
+    {id:2,title:"Xin chao"},
+    {id:1,title:"Learn React Native"},
+    {id:2,title:"Xin chao"},
+    {id:1,title:"Learn React Native"},
+    {id:2,title:"Xin chao"},
+    {id:1,title:"Learn React Native"},
+    {id:2,title:"Xin chao"},
+  ]);
   return (
     <View style={styles.container}>
       <View>
-        <Text style ={styles.text}>{name}</Text>
-        <TextInput style={{
+        
+        <TextInput 
+        onChangeText={value => setName(value)}
+        value={name}
+        autoCapitalize='none'
+        // keyboardType='numeric'
+        // maxLength={2}
+        // multiline={true}
+        autoCorrect={false}
+        style={{
           borderColor:"Violet",
           borderWidth:1
         }}></TextInput>
+        <Text style ={styles.text}>{name}</Text>
+      </View>
+      <Button
+        onPress={()=>alert("tap me")} 
+        title='Add new'/>
+      <View>
+        {todoList.map(todo =>{
+          return(
+              <Text style={styles.todo}>{todo.title}</Text>
+          )
+        })}
       </View>
          <Text  style ={styles.text}>Xin chào các bạn 
             <Text style={styles.dat}>lequangdat</Text>
@@ -34,6 +62,12 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  todo:{
+    fontSize:30,
+    backgroundColor:"pink",
+    marginBottom:10,
+    padding:15,
+  },
   dat:{
     fontSize:30,
     color: "green"
@@ -49,6 +83,7 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
     fontSize:60,color:"red", 
     paddingTop:20,
-    paddingHorizontal:20
+    paddingHorizontal:20,
+    marginTop:50
   },
 });
