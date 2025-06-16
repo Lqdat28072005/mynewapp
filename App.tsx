d4bd3d9947d8c0fe9ff42379f925a64b439d67d5
Lqdat28072005 <23010201@st.phenikaa-uni.edu.vn>
2025-06-16 20:59:37 +0700

    1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //string
+  const[name,setHoidanit] = useState<string>("hoidanit");
+  //number
+  const[age,setAge] = useState<number>(20)
+  //null
+  const test = false;
+
+  //object,array
+  const [person,setPerson] = useState([{
+    name:"hoidanit",
+    age:25
+  }]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Text style ={styles.text}>test</Text>
+        <Text style ={styles.text}>{JSON.stringify(person)}</Text>
+      </View>
+         <Text  style ={styles.text}>Xin chào các bạn 
+            <Text style={styles.dat}>lequangdat</Text>
+         </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  dat:{
+    fontSize:30,
+    color: "green"
+  },
+  text:{
+    fontSize:30,
+    color :"red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize:60,color:"red", 
+    paddingTop:20,
+    paddingHorizontal:20
   },
 });
